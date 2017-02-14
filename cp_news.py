5e5036b52ee8efc568a9356a1dd35bb6f241b77a
ericzuobin <ericzuobin@qq.com>
2017-02-14 14:29:22 +0800

    修改山东体彩公告

--- cp_news.py
+++ cp_news.py
@@ -10,12 +10,14 @@ import traceback
 log_path = '/Users/sahinn/Documents/workspace_py/cp_news/cp_news.log'
 
 
-def url_get(url, timeout=30):
+def url_get(url, timeout=30, encoding="utf8"):
     i_headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
                  "Referer": 'http://www.baidu.com'}
     req = urllib2.Request(url, headers=i_headers)
     result = urllib2.urlopen(req, timeout=timeout)
     content = result.read()
+    if encoding != "utf8":
+        content = unicode(content, encoding).encode("utf8")
     return content
 
 
@@ -60,15 +62,13 @@ def zhtc_zzgg_parser():
 
 def sdtc_tcgz_parser():
     try:
-        base_url = 'http://www.lottery.gov.cn'
-        url = 'http://www.lottery.gov.cn/tzgg/index.html'
-        ul_reg = u'<div class="main_l">[\s\S]*?<\/div>'
-        li_reg = u'<li><span>\((.*?)\)<\/span><a.*?<\/a><a.*?href="(.*?)">(.*?)<\/a><\/li>'
-        content = url_get(url)
-        ul_group = re.findall(ul_reg, content, re.S | re.M)
-        li_group = re.findall(li_reg, ul_group[0], re.S | re.M)
-        for li_line in li_group:
-            print unicode(base_url + li_line[1], 'utf-8'), unicode(li_line[2], 'utf-8'), unicode(li_line[0], 'utf-8')
+        base_url = 'http://www.sdticai.com/'
+        url = 'http://www.sdticai.com/news.asp?dlei=4'
+        td_reg = u'<td.*?class="dbk".*?href="(.*?)".*?<span.*?">(.*?)<\/span>.*?<\/td>[\s\S]*?<td.*?class="dbk".*?">\[(.*?)\]<\/span><\/td>'
+        content = url_get(url, encoding='gb2312')
+        td_group = re.findall(td_reg, content, re.S | re.M)
+        for li_line in td_group:
+            print unicode(base_url + li_line[0], 'utf-8'), unicode(li_line[1], 'utf-8'), unicode(li_line[2], 'utf-8')
             check_save()
     except :
         log_record()
@@ -83,7 +83,7 @@ def send_mail():
 
 
 def main():
-    zhtc_zzgg_parser()
+    sdtc_tcgz_parser()
     send_mail()
 
 
