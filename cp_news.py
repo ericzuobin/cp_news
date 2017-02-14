747faad106ec402b159b68ee6d5abd848271ed11
ericzuobin <ericzuobin@qq.com>
2017-02-14 11:59:32 +0800

    添加山东体彩公告

--- cp_news.py
+++ cp_news.py
@@ -5,6 +5,9 @@
 # @File    : cp_news.py
 import re
 import urllib2
+import traceback
+
+log_path = '/Users/sahinn/Documents/workspace_py/cp_news/cp_news.log'
 
 
 def url_get(url, timeout=30):
@@ -16,30 +19,59 @@ def url_get(url, timeout=30):
     return content
 
 
+def log_record():
+    f = open(log_path, 'a')
+    traceback.print_exc(file=f)
+    f.flush()
+    f.close()
+
+
 def zhcw_zygg_parser():
-    base_url = 'http://www.zhcw.com'
-    url = 'http://www.zhcw.com/xinwen/zhongyaogonggao/'
-    ul_reg = u'<ul class="Nlistul">[\s\S]*?<\/ul>'
-    li_reg = u'<li><span class="Nlink">.*?href="(.*?)".*?>(.*?)<\/a>[\s\S]*?Ntime">(.*?)</span><\/li>'
-    content = url_get(url)
-    ul_group = re.findall(ul_reg, content, re.S | re.M)
-    li_group = re.findall(li_reg, ul_group[0], re.S | re.M)
-    for li_line in li_group:
-        print unicode(base_url + li_line[0], 'utf-8'), unicode(li_line[1], 'utf-8'), unicode(li_line[2], 'utf-8')
-        check_save()
+    try:
+        base_url = 'http://www.zhcw.com'
+        url = 'http://www.zhcw.com/xinwen/zhongyaogonggao/'
+        ul_reg = u'<ul class="Nlistul">[\s\S]*?<\/ul>'
+        li_reg = u'<li><span class="Nlink">.*?href="(.*?)".*?>(.*?)<\/a>[\s\S]*?Ntime">(.*?)</span><\/li>'
+        content = url_get(url)
+        ul_group = re.findall(ul_reg, content, re.S | re.M)
+        li_group = re.findall(li_reg, ul_group[0], re.S | re.M)
+        for li_line in li_group:
+            print unicode(base_url + li_line[0], 'utf-8'), unicode(li_line[1], 'utf-8'), unicode(li_line[2], 'utf-8')
+            check_save()
+    except :
+        log_record()
 
 
 def zhtc_zzgg_parser():
-    base_url = 'http://www.lottery.gov.cn'
-    url = 'http://www.lottery.gov.cn/tzgg/index.html'
-    ul_reg = u'<div class="main_l">[\s\S]*?<\/div>'
-    li_reg = u'<li><span>\((.*?)\)<\/span><a.*?<\/a><a.*?href="(.*?)">(.*?)<\/a><\/li>'
-    content = url_get(url)
-    ul_group = re.findall(ul_reg, content, re.S | re.M)
-    li_group = re.findall(li_reg, ul_group[0], re.S | re.M)
-    for li_line in li_group:
-        print unicode(base_url + li_line[1], 'utf-8'), unicode(li_line[2], 'utf-8'), unicode(li_line[0], 'utf-8')
-        check_save()
+    try:
+        base_url = 'http://www.lottery.gov.cn'
+        url = 'http://www.lottery.gov.cn/tzgg/index.html'
+        ul_reg = u'<div class="main_l">[\s\S]*?<\/div>'
+        li_reg = u'<li><span>\((.*?)\)<\/span><a.*?<\/a><a.*?href="(.*?)">(.*?)<\/a><\/li>'
+        content = url_get(url)
+        ul_group = re.findall(ul_reg, content, re.S | re.M)
+        li_group = re.findall(li_reg, ul_group[0], re.S | re.M)
+        for li_line in li_group:
+            print unicode(base_url + li_line[1], 'utf-8'), unicode(li_line[2], 'utf-8'), unicode(li_line[0], 'utf-8')
+            check_save()
+    except :
+        log_record()
+
+
+def sdtc_tcgz_parser():
+    try:
+        base_url = 'http://www.lottery.gov.cn'
+        url = 'http://www.lottery.gov.cn/tzgg/index.html'
+        ul_reg = u'<div class="main_l">[\s\S]*?<\/div>'
+        li_reg = u'<li><span>\((.*?)\)<\/span><a.*?<\/a><a.*?href="(.*?)">(.*?)<\/a><\/li>'
+        content = url_get(url)
+        ul_group = re.findall(ul_reg, content, re.S | re.M)
+        li_group = re.findall(li_reg, ul_group[0], re.S | re.M)
+        for li_line in li_group:
+            print unicode(base_url + li_line[1], 'utf-8'), unicode(li_line[2], 'utf-8'), unicode(li_line[0], 'utf-8')
+            check_save()
+    except :
+        log_record()
 
 
 def check_save():
